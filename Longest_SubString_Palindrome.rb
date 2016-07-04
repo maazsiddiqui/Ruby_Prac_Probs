9271612d339f742f295b9e69014460980ff0d2f4
Maaz Siddiqui <maazsiddiqui@users.noreply.github.com>
2016-07-04 01:04:38 -0400

    Update and rename Problem (18).rb to Longest_SubString_Palindrome.rb

---
+++ Longest_SubString_Palindrome.rb
@@ -0,0 +1,76 @@
+# Write a method that takes in a string of lowercase letters (no
+# uppercase letters, no repeats). Consider the *substrings* of the
+# string: consecutive sequences of letters contained inside the string.
+# Find the longest such string of letters that is a palindrome.
+#
+# Note that the entire string may itself be a palindrome.
+#
+# You may want to use Array's `slice(start_index, length)` method,
+# which returns a substring of length `length` starting at index
+# `start_index`:
+#
+#     "abcd".slice(1, 2) == "bc"
+#     "abcd".slice(1, 3) == "bcd"
+#     "abcd".slice(2, 1) == "c"
+#     "abcd".slice(2, 2) == "cd"
+#
+# Difficulty: hard.
+
+def palindrome?(string)
+  i = 0
+  while i < string.length
+    if string[i] != string[(string.length - 1) - i]
+      return false
+    end
+
+    i += 1
+  end
+
+  return true
+end
+
+def longest_palindrome(string)
+  
+  newString = ""
+  
+  idx = 0
+  
+  while idx < string.length
+  
+    idx2 = 2
+    
+    while idx2 <= string.length
+    
+      if palindrome?(string.slice(idx, idx2)) && newString.length < (string.slice(idx, idx2)).length
+      
+        newString = string.slice(idx, idx2)
+
+      end
+    
+      idx2 += 1
+    
+    end
+  
+    idx += 1
+  
+  end
+  
+  return newString
+  
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts(
+  'longest_palindrome("abcbd") == "bcb": ' +
+  (longest_palindrome('abcbd') == 'bcb').to_s
+)
+puts(
+  'longest_palindrome("abba") == "abba": ' +
+  (longest_palindrome('abba') == 'abba').to_s
+)
+puts(
+  'longest_palindrome("abcbdeffe") == "effe": ' +
+  (longest_palindrome('abcbdeffe') == 'effe').to_s
+)
