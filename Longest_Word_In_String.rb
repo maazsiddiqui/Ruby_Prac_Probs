fd3649c01b059743e4a155b94defb04aaf0eb853
Maaz Siddiqui <maazsiddiqui@users.noreply.github.com>
2016-07-04 00:41:37 -0400

    Update and rename Problem (3).rb to Longest_Word_In_String.rb

--- Longest_Word_In_String.rb
+++ Longest_Word_In_String.rb
@@ -11,7 +11,6 @@ def longest_word(sentence)
     sentence_split = sentence.split
    
     word = ""
-    
     idx = 0
     
     while idx < sentence_split.length
@@ -23,48 +22,6 @@ def longest_word(sentence)
     
     return word
 	
-end
-
-# These are tests to check that your code is working. After writing
-# your solution, they should all print true.
-
-puts(
-  'longest_word("short longest") == "longest": ' +
-  (longest_word('short longest') == 'longest').to_s
-)
-puts(
-  'longest_word("one") == "one": ' +
-  (longest_word('one') == 'one').to_s
-)
-puts(
-  'longest_word("abc def abcde") == "abcde": ' +
-  (longest_word('abc def abcde') == 'abcde').to_s
-)
-
-
-
-Web Answer
-
-
-def longest_word(sentence)
-  words = sentence.split(" ")
-
-  longest_word = nil
-
-  word_idx = 0
-  while word_idx < words.length
-    current_word = words[word_idx]
-
-    if longest_word == nil
-      longest_word = current_word
-    elsif longest_word.length < current_word.length
-      longest_word = current_word
-    end
-
-    word_idx += 1
-  end
-
-  return longest_word
 end
 
 # These are tests to check that your code is working. After writing
